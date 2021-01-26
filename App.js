7327e24cf51a97eaee57072ff800e8203e531e38
Emiel de Jong <emieldejong@xs4all.nl>
2021-01-26 18:52:38 +0100

    update

--- App.js
+++ App.js
@@ -1,21 +1,58 @@
-import { StatusBar } from 'expo-status-bar';
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import axios from 'axios'
+import React, { useEffect, useState } from 'react'
+import { View, StyleSheet } from 'react-native'
 
-export default function App() {
+function Item(props) {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+    <View style={styles.item}>
+      <Text>{props.item}</Text>
     </View>
-  );
+  )
+}
+
+
+
+export default function FlatList() {
+
+  const [data, setData] = useState([
+    'Ewoud',
+    'Ozgur',
+    'Emiel'
+  ])
+
+  useEffect(() => {
+
+    fetchData()
+    async function fetchData() {
+      try {
+        const result = await axios.get('https://api.chucknorris.io/jokes/categories')
+        console.log(result)
+        setData(result.data)
+      } catch (error) {
+        console.log(error)
+      }
+
+
+    }
+
+  }, [])
+
+  const renderItem = ({ item }) => {
+    return (
+      <Item item={item} />
+    )
+  }
+  return (
+    <View style={{ flex: 1 }}>
+      <FlatList data={data} renderItem={renderItem} />
+    </View>
+  )
 }
 
 const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
+  item: {
+    padding: 50,
+    borderBottomColor: 'black',
+    borderBottomWidth: 2
+  }
+})
